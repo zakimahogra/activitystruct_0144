60f1be6d55f35d9094a1ebf39031b92962d0b7d9
M S I <M S I@MSI>
2024-04-01 13:54:55 +0700

    struktur detail alamat

--- Struktur_3/Struktur_3.cpp
+++ Struktur_3/Struktur_3.cpp
@@ -7,35 +7,4 @@ struct DetailAlamat {
     string kota;
 };
 
-struct Mahasiswa {
-    string nim;
-    string nama;
-    DetailAlamat alamat;
-    string umur;
 
-};
-
-int main() {
-    Mahasiswa mhs;
-    cout << "Nomor Mahasiswa : ";
-    getline(cin ,mhs.nim);
-    cout << "Nama Mahasiswa : ";
-    getline(cin,mhs.nama);
-
-    cout << "Alamat Mahasiswa : " << endl;
-    cout << "\t Nama Desa : ";
-    cin >> mhs.alamat.desa;
-    cout << "\t Nama Kota : ";
-    cin >> mhs.alamat.kota;
-
-    cout << "Umur Mahasiswa : ";
-    cin >> mhs.umur;
-
-    cout << endl;
-    cout << "\n NIM : " << mhs.nim;
-    cout << "\n Nama : " << mhs.nama;
-    cout << "\n Alamat : ";
-    cout << " \n \t Desa : " << mhs.alamat.desa;
-    cout << "\n \t Kota : " << mhs.alamat.kota;
-    cout << "\n Umur : " << mhs.umur;
-}
