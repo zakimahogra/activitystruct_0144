f10f8a2c6b571fdd8a50988993a781f590b7d6fe
M S I <M S I@MSI>
2024-04-01 13:50:27 +0700

    tampilkan nim,nama,alamat,umur

--- Struktur/Struktur.cpp
+++ Struktur/Struktur.cpp
@@ -19,4 +19,9 @@ int main() {
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
 
-    
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
+}
